8799c8338f19c5dbe6103a1fad5ad40ce8f2894f
tonywang10101 <tony.wang.10101@gmail.com>
2024-01-23 15:16:36 +0000

    feat(model): update to python-sdk 0.6.0

--- zephyr_7b/1/model.py
+++ zephyr_7b/1/model.py
@@ -52,9 +52,9 @@ class Zephyr:
 
         print(f"torch.cuda.is_available() : {torch.cuda.is_available()}")
         print(f"torch.cuda.device_count() : {torch.cuda.device_count()}")
-        print(f"torch.cuda.current_device() : {torch.cuda.current_device()}")
-        print(f"torch.cuda.device(0) : {torch.cuda.device(0)}")
-        print(f"torch.cuda.get_device_name(0) : {torch.cuda.get_device_name(0)}")
+        # print(f"torch.cuda.current_device() : {torch.cuda.current_device()}")
+        # print(f"torch.cuda.device(0) : {torch.cuda.device(0)}")
+        # print(f"torch.cuda.get_device_name(0) : {torch.cuda.get_device_name(0)}")
 
         self.pipe = transformers.pipeline(
             "text-generation",
